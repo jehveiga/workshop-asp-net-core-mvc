6295b8e90c0b5f618da26f367694cffc14aa9bf4
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-03-25 18:53:29 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
