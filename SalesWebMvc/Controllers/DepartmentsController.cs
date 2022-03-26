a7871f185c65dd70910122bf31103e5f9a370106
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-03-26 11:06:28 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Electronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
