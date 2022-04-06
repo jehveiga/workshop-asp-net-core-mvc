fa94065c1d5e26d94f092c4333637d4e11cf94ef
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-04-05 23:05:10 -0300

    SellersController

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -4,7 +4,7 @@ using SalesWebMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseMySql("server=localhost;initial catalog=saleswebmvcappdb;uid=developer;password=12345", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql")));
+    options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"), Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -26,7 +26,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
